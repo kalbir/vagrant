2c35526a8bea0673fe3a50eadaae594bf372fa59
Kalbir Sohi <kalbir.sohi@digital.cabinet-office.gov.uk>
2014-03-08 15:29:19 +0000

    Changes to enable docker install

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y nginx
+apt-get install -y linux-image-generic-lts-raring linux-headers-generic-lts-raring curl vim nginx 
 rm -rf /usr/share/nginx/www
 ln -fs /vagrant /usr/share/nginx/www
 nginx
